dc2ffd5a52283ce83647d7efdef9b8e3d271e004
SatriaSuprapto <32392756+SatriaSuprapto@users.noreply.github.com>
2018-04-02 22:16:55 +0700

    membuat atribut dan behavior

---
+++ src/praktek02/persegi.java
@@ -0,0 +1,18 @@
+
+package praktek02;
+
+public class persegi {
+    
+    double sisi1;
+    double sisi2;
+    
+    void.cetakinfo(){
+        
+        System.out.println("==============");
+        System.out.println("sisi1 :"+sisi1);
+        System.out.println("sisi2 :"+sisi2);
+        System.out.println("==============");
+        
+    }
+    
+}
